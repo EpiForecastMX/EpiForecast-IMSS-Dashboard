f913899bf6b0ec9db0e8d898e15d5f4ca2852f77
Javier Rebull <168046724+jrebull@users.noreply.github.com>
2026-03-07 16:01:36 -0500

    feat: header y stats en una sola linea
    - Stats cards eliminados como seccion separada
    - 4 valores (Modelos, SMAPE, Motor, 52 sem) inline en el header
    - Ocultos en pantallas menores a 900px
    - Mas espacio vertical para chat y quick queries

--- kb/js/app.js
+++ kb/js/app.js
@@ -6,7 +6,7 @@
  * y fallback a Gemini via Netlify Function.
  */
 
-import { loadKnowledge, getStats, getData, answer } from './kb.js?v=8';
+import { loadKnowledge, getStats, getData, answer } from './kb.js?v=9';
 
 // ---------------------------------------------------------------------------
 // DOM refs
